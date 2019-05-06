15bc6c2cbcf5c998966fa7e1f94cc9ef3726c698
Yuan Chen <yca316@sfu.ca>
2019-05-05 20:55:24 -0700

    Flipping an image

---
+++ 14.longest-common-prefix.cpp
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=14 lang=cpp
+ *
+ * [14] Longest Common Prefix
+ */
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+    }
+};
+
