7b8ac6725947915fa5ac634304bb5807c81bbd21
Yuan Chen <yca316@sfu.ca>
2019-09-08 17:06:22 -0700

    Update addTwoNumbers

--- KC/2.add-two-numbers.cpp
+++ KC/2.add-two-numbers.cpp
@@ -11,41 +11,49 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
+
+class Solution
+{
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-
-        int index = 0; int first, second;
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        int index = 0;
+        int first, second;
         int sum = 0, carryOver = 0;
-
-        while (l1!= NULL || l2!= NULL) {
-
+        ListNode *ret = NULL;
+        ListNode **tail = &ret;
+        while (l1 != NULL || l2 != NULL)
+        {
             first = getNext(l1);
             second = getNext(l2);
-            sum = first + second;
-            if (sum >= 10) {
-                carryOver = 1;
-                sum -= 10;
-            }
-            ListNode* ret = new ListNode(sum);
-            if (carry != 0) {
-                ret->val += 1;
-                carry = 0;
-            }
-            
-    
+
+            sum = first + second + carryOver;
+
+            ListNode *node = new ListNode(sum % 10);
+            *tail = node;
+            tail = (&node->next);
+
+            carryOver = sum / 10;
         }
+
+        if (carryOver > 0)
+        {
+            ListNode *node = new ListNode(carryOver % 10);
+            *tail = node;
+        }
+
         return ret;
     }
 
-    int getNext(ListNode * &node) {
+    int getNext(ListNode *&node)
+    {
         int x;
-        if ( node != NULL ) {
+        if (node != NULL)
+        {
             x = node->val;
             node = node->next;
         }
-        
+
         return x;
     }
 };
-
