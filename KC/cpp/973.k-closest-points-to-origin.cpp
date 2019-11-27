c14bfc451f246433f5f3fed15f46f4e15eaa60fe
Karen Chen <karencyuan@gmail.com>
2019-11-26 23:30:11 -0800

    Add 973.

---
+++ KC/cpp/973.k-closest-points-to-origin.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=973 lang=cpp
+ *
+ * [973] K Closest Points to Origin
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        vector<vector<int>> result;
+        multimap<double, vector<int>, less<long long>> mapOfItems;
+        int i = 0;
+
+        for (auto const &point : points)
+        {
+            const double euclideanDistance = sqrt(pow(point[0], 2) + pow(point[1], 2));
+            mapOfItems.insert(make_pair(euclideanDistance, point));
+        }
+
+        for (auto const &point : mapOfItems)
+        {
+            if (i < K)
+            {
+                cout << point.first << endl;
+                result.push_back(point.second);
+                i++;
+            }
+        }
+
+        return result;
+    };
+};
+// @lc code=end
+
